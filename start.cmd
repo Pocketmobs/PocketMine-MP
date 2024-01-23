602f2fc5c1329f0928cdf99876a458a8dea64525
John C Hofbauer <98779922+JohnHofbauer@users.noreply.github.com>
2024-01-23 14:40:57 -0500

    adding the plugins as submodules

--- start.cmd
+++ start.cmd
@@ -1,6 +1,13 @@
 @echo off
+setlocal enabledelayedexpansion
+
 TITLE PocketMine-MP server software for Minecraft: Bedrock Edition
 cd /d %~dp0
+
+REM Define the list
+set "PLUGIN_PATH[0]=Claims"
+set "PLUGIN_PATH[1]=PokeSidebar"
+set "PLUGIN_PATH[2]=PokemonUI"
 
 set PHP_BINARY=
 
@@ -30,6 +37,14 @@ if exist PocketMine-MP.phar (
 	pause
 	exit 1
 )
+
+REM Iterate through the list
+for /L %%i in (0, 1, 2) do (
+	echo Item %%i: !PLUGIN_PATH[%%i]!
+	%PHP_BINARY% -dphar.readonly=0 pharynx.phar -c -i plugin_source/!PLUGIN_PATH[%%i]! -p=plugins\!PLUGIN_PATH[%%i]!.phar
+)
+
+%PHP_BINARY% -dphar.readonly=0 bootstrap-plugin-dev.php plugins\pharynx-output.phar
 
 if exist bin\mintty.exe (
 	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
